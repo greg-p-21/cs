93757448c3c2eaa006fbfa83f8e09cc18fbdd223
Greg Polmatier <m215394@usna.edu>
2021-02-28 22:14:11 -0500

    prj1 phase 1 and 2

--- Proj01/client.js
+++ Proj01/client.js
@@ -1,12 +1,28 @@
 // const { listen } = require("engine.io");
 
+// const { listen } = require("engine.io");
+
 // Gregory Polmatier
 var socket = io();
 
+// timer
+const ALLOTED_TIME = 60;
+
+var timer = null;
+var timeLeft = ALLOTED_TIME;
+
+var t1score = 0;
+var t2score = 0;
+
+var t1scoreBoard = null;
+var t2scoreBoard = null;
+
+var randomWord = null;
+var words = [];
 
 // Draw on Canvas Starter Code From:
 // https://stackoverflow.com/questions/2368784/draw-on-html5-canvas-using-a-mouse
-var canvas, ctx, flag = false,
+var canvas, guessCanvas, guessCTX, ctx, flag = false,
     drawStartX = 0,
     drawCurrentX = 0,
     drawStartY = 0,
@@ -17,13 +33,27 @@ var color = "black",
     strokeWidth = 2;
 
 function init() {
+    document.getElementById("startButton").style.display = "none";
+    t1scoreBoard = document.getElementById("t1score");
+    t2scoreBoard = document.getElementById("t2score");
+
+    randomWord = document.getElementById("randomWord");
+
+    // for right now
+    document.getElementById("phase2").style.display = "none";
+    document.getElementById("drawer").style.display = "none";
+    document.getElementById("guesser").style.display = "none";
+
+
     /* canvas stuff */
     canvas = document.getElementById('paintCanvas');
+    guessCanvas = document.getElementById('displayCanvas');
+    guessCTX = guessCanvas.getContext("2d");
     ctx = canvas.getContext("2d");
 
     // Not used
-    // w = canvas.width;
-    // h = canvas.height;
+    w = canvas.width;
+    h = canvas.height;
 
     canvas.addEventListener("mousemove", function (event) {
         findxy('move', event)
@@ -39,41 +69,285 @@ function init() {
     }, false);
     /********************************/
 
+    document.getElementById("startButton").onclick = function() {
+        socket.emit("startHit");
+    };
+
+    document.getElementById("correct").onclick = function() {
+        socket.emit("correct");
+        words.pop();
+    };
+
+    document.getElementById("skipButton").onclick = function() {
+        randomWord.innerHTML = words.pop();
+        socket.emit("skip");
+    };
 }
 
-// vvvvvvvvvvvvvvvvvvvvv   Setting Name   vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-function submitName() {
-    myName = document.getElementById("name").value;
-    isPlaying = true;
-
-    document.getElementById("enterNameDiv").style.display = "none";
-    document.getElementById("phase1").style.display = "block";
-
-    socket.emit('setName', myName);
-}
-
-socket.on('teamLists', function (team1, team2, unass) {
-    var t1list = document.getElementById("t1list");
-    t1list.innerHTML = "";
-    addTeamList(team1, t1list);
-
-    var t2list = document.getElementById("t2list");
-    t2list.innerHTML = "";
-    addTeamList(team2, t2list);
-    
-
-    var unlist = document.getElementById("unlist");
-    unlist.innerHTML = "";
-    addTeamList(unass, unlist);
+socket.on("clear", function() {
+    console.log("clear");
+    clearUL("guessList");
+    erase();
 });
 
 function addTeamList(team, list) {
+    console.log("addTeamList");
     team.forEach(function(player) {
         var entry = document.createElement('li');
+        entry.draggable = "true";
+        entry.class = "dropzone";
+
+        entry.id = player.name;
         entry.appendChild(document.createTextNode(player.name));
         list.appendChild(entry);
     });
 }
+
+socket.on("showStart", function() {
+    console.log("showstart");
+    document.getElementById("startButton").style.display = "inline";
+});
+
+socket.on("startGame", function(ws, drawSockets) {
+    console.log("client game started");
+    // hide team select and show appropriate phase II
+    document.getElementById("teamSelect").style.display= "none";
+    document.getElementById("phase2").style.display = "inline";
+
+    if (drawSockets.includes(socket.id)) {
+        document.getElementById("drawer").style.display = "inline";
+    } else {
+        document.getElementById("guesser").style.display = "inline";
+    }
+
+    // set words
+    document.getElementById("timer").innerHTML = `Time Remaining: ${ALLOTED_TIME}`;
+
+    // start timer
+    timer = setInterval(countDown, 1000);
+
+    // get words
+    words = ws;
+
+    if (drawSockets.includes(socket.id)) {
+        // display drawer div
+        document.getElementById("drawer").style.display = "inline";
+        randomWord.innerHTML = words.pop();
+    } else {
+        // if guesser
+        document.getElementById("guesser").style.display = "inline";
+    }
+});
+
+function submitGuess() {
+
+    // get guess
+    var guess = document.getElementById("guessText").value;
+
+    // send guess to server
+    console.log("submitguess client:", guess);
+    socket.emit("newGuess", guess);
+
+    //clear guess
+    guess.innerHTML = "";
+}
+
+socket.on("guessAttempt", function (guess) {
+    console.log("guessattempt client", guess);
+
+    list = document.getElementById("guessList");
+    var entry = document.createElement('li');
+    entry.class = "guess";
+    entry.id = guess;
+    entry.appendChild(document.createTextNode(guess));
+    list.appendChild(entry);
+});
+
+socket.on("updateScore", function(t1s, t2s) {
+    console.log("updateScore");
+
+    t1scoreBoard.innerHTML = t1s;
+    t2scoreBoard.innerHTML = t2s;
+
+    // clear
+});
+
+socket.on("newDraw", function(dsx, dsy, dcx, dcy, coler) {
+    console.log("newDraw");
+
+    drawStartX = dsx;
+    drawStartY = dsy;
+    drawCurrentX = dcx;
+    drawCurrentY = dcy;
+    color = coler;
+
+    guessDraw();
+});
+
+function countDown() {
+    console.log("countdown");
+
+
+    var time = document.getElementById("timer");
+    time.innerHTML = "Time Remaining: " + --timeLeft;
+    if (timeLeft == 0) {
+        timeLeft = ALLOTED_TIME;
+        time.innerHTML = "Time Remaining: " + timeLeft;
+
+        clearTimeout(timer);
+
+        socket.emit("gameover")
+    }
+}
+
+/****************** Drag code *************************/
+
+// https://stackoverflow.com/questions/12332403/html5-ul-li-draggable
+let itemBeingDragged;
+let idOfDraggedItem;
+let index;
+let indexDrop;
+let list;
+
+function addPlayer() {
+    console.log("addPlayer");   
+    var list = document.getElementById("unassigned");
+    var playerLI = document.createElement("LI");
+    playerLI.draggable = "true";
+    playerLI.className = "dropzone";
+
+    // Change id and innerHTML to make sense
+    var name = document.getElementById("name").value;
+    console.log(name);
+    playerLI.id = name;
+    playerLI.innerHTML = name;
+    list.appendChild(playerLI);
+    socket.emit("setName", name);
+    console.log("name sent", name);
+  
+}
+
+function createListItem(listname, val) {
+
+    var list = document.getElementById(listname);
+    var playerLI = document.createElement("LI");
+    playerLI.draggable = "true";
+    playerLI.className = "dropzone";
+
+    // Change id and innerHTML to make sense
+    var name = val;
+    console.log("createListItem", name);
+    playerLI.id = name;
+    playerLI.innerHTML = name;
+    list.appendChild(playerLI);
+}
+
+function addDragItem(listname) {
+    var list = document.getElementById(listname);
+    var playerLI = document.createElement("LI");
+    // playerLI.draggable = "false";
+    playerLI.setAttribute('draggable', false);
+    playerLI.className = "dropzone";
+    playerLI.innerHTML = "Drag Here to Add!";
+    console.log("addDragItem draggable", playerLI.draggable);
+    list.appendChild(playerLI);
+}
+
+function clearUL(listname) {
+    var list = document.getElementById(listname);
+    console.log(list)
+    while (list.firstChild) {
+        list.removeChild(list.firstChild);
+    }
+ }
+
+
+socket.on('teamLists', function (team1, team2, unass) {
+    clearUL("team1");
+    clearUL("team2");
+    clearUL("unassigned");
+
+
+    console.log("teams", team1, team2, unass);
+
+    console.log("Team 1");
+    for (var i = 0; i < team1.length; i++) {
+        console.log("team1", team1[i])
+        createListItem("team1", team1[i]);
+    }
+    console.log("Team 2");
+    for (var i = 0; i < team2.length; i++) {
+        // console.log(team2List.children[i].innerHTML);
+        console.log("team2", team2[i])
+        createListItem("team2", team2[i]);
+    }
+    console.log("Unassigned");
+    for (var i = 0; i < unass.length; i++) {
+        // console.log(team2List.children[i].innerHTML);
+        console.log("unass", unass[i])
+        createListItem("unassigned", unass[i]);
+    }
+    addDragItem("team1");
+    addDragItem("team2");
+    // addDragItem("unassigned");
+});
+
+function getTeamFromList(listname) {
+    var list = document.getElementById(listname);
+    teamlist = [];
+    for (var i = 0; i < list.children.length; i++) {
+        var child = list.children[i];
+        console.log("draggable", child.draggable);
+        if (child.draggable) {
+            console.log("getTeamFromList", child.innerHTML);
+            teamlist.push(child.innerHTML);
+        }
+    }
+    return teamlist;
+  }
+
+function changeTeam() {
+    team1 = getTeamFromList("team1");
+    team2 = getTeamFromList("team2");
+    unass = getTeamFromList("unassigned");
+
+    socket.emit("changeTeam", team1, team2, unass);
+}
+
+
+document.addEventListener("dragstart", function ({ target }) {
+  itemBeingDragged = target;
+  idOfDraggedItem = target.id;
+  list = target.parentNode.children;
+  for (let i = 0; i < list.length; i += 1) {
+    if (list[i] === itemBeingDragged) {
+      index = i;
+    }
+  }
+});
+
+document.addEventListener("dragover", function (event) {
+  event.preventDefault();
+});
+
+document.addEventListener("drop", function ({ target }) {
+  if (target.className == "dropzone" && target.id !== idOfDraggedItem) {
+    itemBeingDragged.remove(itemBeingDragged);
+    for (let i = 0; i < list.length; i += 1) {
+      if (list[i] === target) {
+        indexDrop = i;
+      }
+    }
+    console.log(index, indexDrop);
+    if (index > indexDrop) {
+      target.before(itemBeingDragged);
+    } else {
+      target.after(itemBeingDragged);
+    }
+    changeTeam();
+  }
+});
+
 
 function setColor(colorSelectBox) {
     switch (colorSelectBox.id) {
@@ -112,14 +386,26 @@ function draw() {
     ctx.lineWidth = strokeWidth;
     ctx.stroke();
     ctx.closePath();
+    socket.emit("draw", drawStartX, drawStartY, drawCurrentX, drawCurrentY, color);
+}
+
+function guessDraw() {
+    guessCTX.beginPath();
+    guessCTX.moveTo(drawStartX, drawStartY);
+    guessCTX.lineTo(drawCurrentX, drawCurrentY);
+    guessCTX.strokeStyle = color;
+    guessCTX.lineWidth = strokeWidth;
+    guessCTX.stroke();
+    guessCTX.closePath();
 }
 
 function erase() {
-    var m = confirm("Want to clear");
-    if (m) {
-        ctx.clearRect(0, 0, w, h);
-        document.getElementById("canvasimg").style.display = "none";
-    }
+    // var m = confirm("Want to clear");
+    // if (m) {
+    ctx.clearRect(0, 0, w, h);
+    guessCTX.clearRect(0, 0, w, h);
+    document.getElementById("canvasimg").style.display = "none";
+    // }
 }
 
 function save() {
--- Proj01/server.js
+++ Proj01/server.js
@@ -4,6 +4,10 @@ const io = require('socket.io')(http);
 const fs = require('fs')
 const port = process.env.PORT || 15394;
 
+readFromFile("words.txt");
+
+var readline = require('readline');
+
 const TEAM_UN = 0;
 const TEAM_1  = 1;
 const TEAM_2  = 2;
@@ -12,114 +16,157 @@ var team1 = [];
 var team2 = [];
 var unass = [];
 
+var team1ids = [];
+var team2ids = [];
+
+var t1drawer = null;
+var t2drawer = null;
+
+var sockets = {};
+
+var t1score = 0;
+var t2score = 0; 
+
+var words = [];
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 app.get('/client.js', function (req, res) {
     res.sendFile(__dirname + '/client.js');
 });
-// app.get('/favicon.png', function (req, res) {
-//     res.sendFile(__dirname + '/favicon.png');
-// });
+app.get('/favicon.jpeg', function (req, res) {
+    res.sendFile(__dirname + '/favicon.jpeg');
+});
 app.get('/style.css', function (req, res) {
     res.sendFile(__dirname + '/style.css');
 });
+app.get('/words.txt', function (req, res) {
+    res.sendFile(__dirname + '/words.txt');
+});
+
+// var rl = readline.createInterface({
+// 	input: fs.createReadStream('/words.txt'),
+// 	output: process.stdout,
+// 	terminal: false
+// });
+
 
 // I used a class but you could have used arrays, associative arrays, or something else
-class Player {
-    constructor(socketID, name) {
-        this.socketID = socketID;
-        this.name = name;
-        this.active = true;
-    }
-}
+// class Player {
+//     constructor(socketID, name) {
+//         this.socketID = socketID;
+//         this.name = name;
+//         this.active = true;
+//     }
+// }
 
 
 io.on('connection', function (socket) {
     // console.log(socket.id + "connected!");
+    
 
     socket.on('setName', function (name) {
         // Add to players array
-        unass.push(new Player(socket.id, name));
+        unass.push(name);
+        sockets[name] = socket.id;
+        console.log(team1, team2, unass);
         io.emit('teamLists', team1, team2, unass);
     });
 
-    socket.on('start', function () {
-        var randomSeed = Math.floor(Math.random() * 100)
-        // https://stackoverflow.com/questions/7364150/find-object-by-id-in-an-array-of-javascript-objects
-        var playerWhoClickedStart = players.find(player => player.socketID === socket.id).name;
-        io.emit('startedBy', players, playerWhoClickedStart, randomSeed);
-    });
+    socket.on('changeTeam', function (t1, t2, un) {
+        team1 = t1;
+        team2 = t2;
+        unass = un;
+        io.emit("teamLists", team1, team2, unass);
 
-    socket.on('stop', function () {
-        // https://stackoverflow.com/questions/7364150/find-object-by-id-in-an-array-of-javascript-objects
-        var playerWhoHitStop = players.find(player => player.socketID === socket.id)
-        io.emit('stoppedBy', playerWhoHitStop)
-    });
-
-    socket.on('gameMode', function (gameMode) {
-        // Game mode updated
-        io.emit('newGameMode', gameMode)
-    });
-
-    socket.on('hitTarget', function () {
-        // https://stackoverflow.com/questions/7364150/find-object-by-id-in-an-array-of-javascript-objects
-        var playerWhoHitTarget = players.find(player => player.socketID === socket.id)
-        playerWhoHitTarget.score++
-
-        sortPlayers()
-
-        io.emit('scoreUpdate', players);
-    });
-
-    socket.on('gameover', function () {
-        console.log("server got gameover")
-        if (players[0].score != 0) {
-            // https://stackoverflow.com/questions/10645994/how-to-format-a-utc-date-as-a-yyyy-mm-dd-hhmmss-string-using-nodejs
-            appendToFile("highscores.txt", `${players[0].name} - ${players[0].score} @ ${new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')}\n`)
+        console.log(team1.length, team2.length);
+        if (team1.length > 1 && team2.length > 1) {
+            io.emit("showStart");
         }
-        resetScores();
+        team1ids = [];
+        team2ids = [];
 
+        team1.forEach(p => team1ids.push(sockets[p]));
+        team2.forEach(p => team2ids.push(sockets[p]));
     });
 
-    socket.on('disconnect', function () {
+    // PHASE 2 ******************************
 
-        var playerWhoDisconnected = players.find(player => player.socketID === socket.id)
-        console.log("someone disconnected! " + socket.id + " named: " + playerWhoDisconnected);
+    socket.on("startHit", function() {
+        // get words list
+        // words = Array.from(readFromFile('words.txt'));
+        // readFromFile("words.txt");
+        shuffleArray(words);
+        console.log("words", words);
 
-        if (playerWhoDisconnected) {
-            playerWhoDisconnected.score = -1 // sentinel value of -1 means disconnected
-            playerWhoDisconnected.active = false;
+        // pick drawers
+        //https://stackoverflow.com/questions/5915096/get-a-random-item-from-a-javascript-array
+        t1drawer = team1ids[Math.floor(Math.random() * team1ids.length)];
+        t2drawer = team2ids[Math.floor(Math.random() * team2ids.length)];
+        
+        // emit
+        io.emit("startGame", words, [t1drawer, t2drawer]);
+    });
 
-            var activePlayers = players.filter(x => x.active).length
-            if (activePlayers == 0) {
-                clearPlayerArray()
-            }
-            
-            sortPlayers()
-            io.emit('scoreUpdate', players);
+    socket.on("skip", function () {
+        io.emit("clear");
+    })
+
+    socket.on("correct", function() {
+        if (socket.id === t1drawer) {
+            t1score++;
+        } else {
+            t2score++;
+        }
+        io.emit("updateScore", t1score, t2score);
+        io.emit("clear");
+    });
+
+    socket.on("draw", function (dsx, dsy, dcx, dcy, color) {
+        if (socket.id === t1drawer) {
+            team1ids.forEach(id => io.to(id).emit("newDraw", dsx, dsy, dcx, dcy, color));
+        } else {
+            team2ids.forEach(id => io.to(id).emit("newDraw", dsx, dsy, dcx, dcy, color));
         }
     });
+
+    socket.on("newGuess", function (guess) {
+        console.log("newGuess server", guess);
+        // console.log(team1ids, team2ids, sockets, team1);
+        console.log("equal", socket.id === t1drawer, socket.id, t1drawer);
+        if (team1ids.includes(socket.id)) {
+            team1ids.forEach(id => io.to(id).emit("guessAttempt", guess));
+        } else {
+            team2ids.forEach(id => io.to(id).emit("guessAttempt", guess));
+        }
+    })
+
 });
 
-function clearPlayerArray() {
-    players = []
-}
-function resetScores() {
-    for (var i = 0; i < players.length; i++) {
-        players[i].score = 0
-    }
-}
 
-function appendToFile(file, data) {
-    fs.appendFile(file, data, function (err) {
-        if (err) console.log("ERROR writing " + data + ":" + err);
-    });
+function readFromFile(file) {
+	fs.readFile(file, function (err, data) {
+		if (err) throw err;
+        words = String(data).split('\n');
+		console.log(words);
+	});
 }
 
 function sortPlayers() {
     // https://flaviocopes.com/how-to-sort-array-of-objects-by-property-javascript/
     players.sort((a, b) => (a.score < b.score) ? 1 : -1);
+}
+
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+/* Randomize array in-place using Durstenfeld shuffle algorithm */
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
 
 http.listen(port, '0.0.0.0', function () {
